708091dd14ff3be74c42635be7654920e4d0168f
Ebu44 <ebubekiryusuf44@hotmail.com>
2022-02-13 18:51:20 +0300

    product test and model added. Product fixtures added

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -46,3 +46,64 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    """
+    Product details table
+    """
+
+    web_id = models.CharField(
+        verbose_name=_("product website ID"),
+        help_text=_("format: required, unique"),
+        max_length=50,
+        unique=True,
+        null=True,
+        blank=True,
+    )
+
+    slug = models.SlugField(
+        verbose_name=_("product safe URL"),
+        help_text=_("format: required, letters, numbers,underscores or hyphens"),
+        max_length=255,
+        unique=False,
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        verbose_name=_("product name"),
+        help_text=_("format: required, max-255"),
+        max_length=255,
+        unique=False,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        verbose_name=_("product description"),
+        help_text=_("format: required"),
+        unique=False,
+        null=False,
+        blank=False,
+    )
+    category = TreeManyToManyField(Category)
+    is_active = models.BooleanField(
+        verbose_name=_("product visibility"),
+        help_text=_("format: true=product visible"),
+        unique=False,
+        null=False,
+        blank=False,
+        default=True,
+    )
+    created_at = models.DateTimeField(
+        verbose_name=_("date product created"),
+        help_text=_("format: Y-m-d H:M:S"),
+        auto_now_add=True,
+        editable=False,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_("date product last updated"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+
+    def __str__(self):
+        return self.name
